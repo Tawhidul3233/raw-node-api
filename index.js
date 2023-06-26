8ff5ed2dafde6242179352e068bc4b2bfea5af15
Tawhidul3233 <mdtawhidulislam3233@gmail.comm>
2023-06-26 21:32:05 +0600

    server created

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // dependencies
 const http = require('http');
+
+// app object - module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+  port: 5000,
+};
+
+// create server 
+app.createServer = () => {
+  const server = http.createServer(app.handelReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`Server runing ${app.config.port} port`)
+  })
+}
+
+// handel req res 
+app.handelReqRes = (req, res) => {
+  res.end('Hello wold')
+}
+
+// invokd funcation 
+app.createServer();
